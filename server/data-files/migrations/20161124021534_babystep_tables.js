904ade8e7c42e040cd3c35135d61c47653e2b809
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-24 03:38:08 +0000

    knex first migration created for basic database setup, and server.js modified requiring knex

---
+++ server/data-files/migrations/20161124021534_babystep_tables.js
@@ -0,0 +1,43 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.increments();
+      table.string('username');
+      table.string('email');
+      table.string('password');
+    }),
+
+    knex.schema.createTable('maps', function(table) {
+      table.increments();
+      table.string('region');
+      table.string('keyword');
+      table.date('date_created');
+      table.integer('user_id').references('users.id');
+    }),
+
+    knex.schema.createTable('coordinates', function(table) {
+      table.increments();
+      table.decimal('latitude');
+      table.decimal('longitude');
+      table.timestamps();
+      table.integer('map_id').references('maps.id');
+    }),
+
+    knex.schema.createTable('favourites', function(table) {
+      table.increments();
+      table.integer('user_id').references('users.id');
+      table.integer('map_id').references('maps.id');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones'),
+    knex.schema.dropTable('maps'),
+    knex.schema.dropTable('coordinates')
+    knex.schema.dropTable('favourites')
+  ])
+};
+
